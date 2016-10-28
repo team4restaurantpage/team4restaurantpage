ce7c933b0183fcae2714b30cb4ee4ada24ebac1d
Jack Storrs <jackstorrs8@gmail.com>
2016-10-27 22:59:21 -0400

    Finished menu section of main tab. All html is appended in the 'menutab.js' file.

--- src/js/menutab.js
+++ src/js/menutab.js
@@ -7,14 +7,32 @@ function renderMenu (data) {
   console.log(data);
   $('.maintabs').append(
     `<header>Our Dinner Selections...</header>
-      <div id="Beer">Beer Menu</div>`);
+     <div id="Beer">Beer Menu 맥주 메뉴</div>`);
   data.Beer.forEach(function (beer) {
-    $('.maintabs').append(
-      `<div class="beer-selection">${beer.item} - ${beer.style} - ${beer.price}</div>
-      <div class="beer-description">"${beer.description}"</div>`
-    );
-  });
+  $('.maintabs').append(
+    `<div class="beer-selection">${beer.item} - ${beer.style} - ${beer.price}₩</div>
+     <div class="beer-description">${beer.description}</div>`
+   )});
+  $('.maintabs').append(
+    `<div id="entrees">Entrees 앙트레</div>`);
+  data.entrees.forEach(function (entree) {
+  $('.maintabs').append(
+    `<div class="entree-selection">${entree.item} - ${entree.price}₩</div>
+     <div class="entree-description">${entree.description}</div>
+    `
+  )});
+  $('.maintabs').append(
+    `<div id="games">Games 계략</div>
+     <div id="perhour">(prices listed per hour)</div>
+    `);
+  data.games.forEach(function (game) {
+  $('.maintabs').append(
+    `<div class="game-selection">${game.item} - ${game.price}₩</div>
+     <div class="game-description">${game.description}</div>
+    `
+  )});
 };
+
 
 export { renderMenu };
 
