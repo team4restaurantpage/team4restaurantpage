299fc383c28c82c716405e9ade214739aaeb5234
Jack Storrs <jackstorrs8@gmail.com>
2016-10-28 11:47:51 -0400

    changed the container that html was being appended to the div 'menu-content'

--- src/js/menutab.js
+++ src/js/menutab.js
@@ -5,28 +5,28 @@ import { menuAPI } from './restaurantAPI.js'
 
 function renderMenu (data) {
   console.log(data);
-  $('.maintabs').append(
+  $('.menu-content').append(
     `<header>Our Dinner Selections...</header>
      <div id="Beer">Beer Menu 맥주 메뉴</div>`);
   data.Beer.forEach(function (beer) {
-  $('.maintabs').append(
+  $('.menu-content').append(
     `<div class="beer-selection">${beer.item} - ${beer.style} - ${beer.price}₩</div>
      <div class="beer-description">${beer.description}</div>`
    )});
-  $('.maintabs').append(
+  $('.tab.content').append(
     `<div id="entrees">Entrees 앙트레</div>`);
   data.entrees.forEach(function (entree) {
-  $('.maintabs').append(
+  $('.menu-content').append(
     `<div class="entree-selection">${entree.item} - ${entree.price}₩</div>
      <div class="entree-description">${entree.description}</div>
     `
   )});
-  $('.maintabs').append(
+  $('.menu-content').append(
     `<div id="games">Games 계략</div>
      <div id="perhour">(prices listed per hour)</div>
     `);
   data.games.forEach(function (game) {
-  $('.maintabs').append(
+  $('.menu-content').append(
     `<div class="game-selection">${game.item} - ${game.price}₩</div>
      <div class="game-description">${game.description}</div>
     `
