a44cbb06683405a2c6f04689419d485e28b4b1e8
Jack Storrs <jackstorrs8@gmail.com>
2017-02-01 18:31:57 -0500

    make some modifications to the styling. looking much fresher.

--- src/js/click.js
+++ src/js/click.js
@@ -1,19 +1,27 @@
 
 function tabHandler (event) {
   var target= $(event.target);
+  console.log(target);
+  target.removeClass("dormant");
   if (target.is("#story-tab")) {
     $( ".story-content" ).addClass( "cont" );
     $(".menu-content").removeClass("cont");
     $(".reserve-content").removeClass("cont");
+    $("#menu-tab").addClass("dormant");
+    $("#reserve-tab").addClass("dormant");
+    console.log($("#menu-tab"));
   } else if (target.is("#menu-tab")) {
     $( ".menu-content" ).addClass( "cont" );
     $(".story-content").removeClass("cont").addClass("hidden-cont");
     $(".reserve-content").addClass("hidden-cont").removeClass("cont");
+    $("#story-tab").addClass("dormant");
+    $("#reserve-tab").addClass("dormant");
   } else if (target.is("#reserve-tab")) {
     $( ".reserve-content" ).addClass( "cont" );
     $(".story-content").removeClass("cont").addClass("hidden-cont");
     $(".menu-content").removeClass("cont");
-
+    $("#menu-tab").addClass("dormant");
+    $("#story-tab").addClass("dormant");
 }};
 
-$(".each-tab").click(tabHandler)
+export { tabHandler };
